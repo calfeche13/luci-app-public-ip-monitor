18f0a148f15a348c34eb71e156e32b8fed6b1519
chosenrealmalfeche <chosen.alfeche@gmail.com>
2024-09-05 00:39:21 +0800

    Removed history tab and added Info/Overview tab to display current IPv4/IPv6 and history for both ips

--- htdocs/luci-static/resources/view/public_ip_monitor.js
+++ htdocs/luci-static/resources/view/public_ip_monitor.js
@@ -29,11 +29,32 @@ var monitorIpv4 = '0';
 var monitorIpv6 = '0';
 var ipv4ScriptContent = '';
 var ipv6ScriptContent = '';
+var history = {};
+var currentPublicIpv4 = 'N/A';
+var currentPublicIpv6 = 'N/A';
 
 function processTabVisibility(data, node = document) {
   toggleElement(data.monitorIpv4 == '1', "li[data-tab='ipv4']", node);
   toggleElement(data.monitorIpv6 == '1', "li[data-tab='ipv6']", node);
   toggleElement(data.monitorIpv6 == '1' || data.monitorIpv4 == '1', "li[data-tab='history']", node);
+}
+
+function parseHistory(historyArray) {
+  let rs = []
+
+  if (!historyArray) {
+    return rs;
+  }
+
+  for (let i = 0; i < historyArray.length; i++) {
+    let historyData = historyArray[i];
+    rs.push([
+      historyData["change_timestamp"] || "N/A",
+      historyData["from"] || "N/A",
+      historyData["to"] || "N/A",
+    ]);
+  }
+  return rs;
 }
 
 function processPostRendering(data, node = document) {
@@ -43,10 +64,75 @@ function processPostRendering(data, node = document) {
 
   processTabVisibility(data, node);
 
+  var ipv4HistoryTbl = E('table', { 'class': 'table' }, [
+    E('tr', { 'class': 'tr table-titles' }, [
+      E('th', { 'class': 'th', 'style': 'width:36%;' }, [ _('Timestamp') ]),
+      E('th', { 'class': 'th', 'style': 'width:32%;' }, [ _('From IPv4') ]),
+      E('th', { 'class': 'th', 'style': 'width:32%;' }, [ _('To IPv4') ])
+    ])
+  ]);
+
+  var ipv6HistoryTbl = E('table', { 'class': 'table' }, [
+    E('tr', { 'class': 'tr table-titles' }, [
+      E('th', { 'class': 'th', 'style': 'width:36%;' }, [ _('Timestamp') ]),
+      E('th', { 'class': 'th', 'style': 'width:32%;' }, [ _('From IPv6') ]),
+      E('th', { 'class': 'th', 'style': 'width:32%;' }, [ _('To IPv6') ])
+    ])
+  ]);
+
+  cbi_update_table(ipv4HistoryTbl, parseHistory(history["history_ipv4"]),
+    E('em', _('No entries available'))
+  );
+
+  cbi_update_table(ipv6HistoryTbl, parseHistory(history["history_ipv6"]),
+    E('em', _('No entries available'))
+  );
+
+  var info_nodes = [
+    E('div', { 'class': 'cbi-section' }, [
+      E('div', { 'class': 'cbi-value' }, [
+        E('label', { 'class': 'cbi-value-title' }, _('Public IPv4: ')),
+        E('div', { 'class': 'cbi-value-field' }, [
+          E('div', [
+            E('input', { 'style': 'border: 0px; pointer-events: none;', 'value': data.currentPublicIpv4 || 'N/A', 'readonly': 'true' })
+          ])
+        ]),
+        E('label', { 'class': 'cbi-value-title' }, _('Public IPv6: ')),
+        E('div', { 'class': 'cbi-value-field' }, [
+          E('div', [
+            E('input', { 'style': 'border: 0px; pointer-events: none;', 'value': data.currentPublicIpv6 || 'N/A', 'readonly': 'true' })
+          ])
+        ]),
+      ])
+    ])
+  ];
+
+  if (data.monitorIpv4 == '1') {
+    info_nodes.push(
+      E('div', { 'class': 'cbi-section'}, [
+        E('h3', _('Public IPv4 Change History')),
+        ipv4HistoryTbl
+      ])
+    );
+  }
+
+  if (data.monitorIpv6 == '1') {
+    info_nodes.push(
+      E('div', { 'class': 'cbi-section'}, [
+        E('h3', _('Public IPv6 Change History')),
+        ipv6HistoryTbl
+      ])
+    );
+  }
+
+  node
+    .querySelector('div.cbi-section-node#cbi-public_ip_monitor-info')
+    .appendChild(E('p', {}, info_nodes));
+
   node
     .querySelector('div.cbi-section-node#cbi-public_ip_monitor-ipv4')
     .appendChild(E('p', {}, [
-      E('h2', _('IPv4 Script')),
+      E('h3', _('IPv4 Script')),
       E('p', { 'class': 'cbi-section-descr', 'style' : 'margin-bottom: 16px;' } , _('Script to run when the public IPv4 changes, use $1 to retrieve the new IPv4 inside the script.')),
       E('textarea', { 'id': 'ipv4_script', 'style': 'width:100%; margin-bottom: 16px;', 'rows': 25, 'resize': 'none' }, [ data.ipv4ScriptContent == undefined ? '' : data.ipv4ScriptContent ])
     ]));
@@ -54,7 +140,7 @@ function processPostRendering(data, node = document) {
   node
     .querySelector('div.cbi-section-node#cbi-public_ip_monitor-ipv6')
     .appendChild(E('p', {}, [
-      E('h2', _('IPv6 Script')),
+      E('h3', _('IPv6 Script')),
       E('p', { 'class': 'cbi-section-descr', 'style' : 'margin-bottom: 16px;' } , _('Script to run when the public IPv6 changes, use $1 to retrieve the new IPv6 inside the script.')),
       E('textarea', { 'id': 'ipv6_script', 'style': 'width:100%; margin-bottom: 16px;', 'rows': 25, 'resize': 'none' }, [ data.ipv6ScriptContent == undefined ? '' : data.ipv6ScriptContent ])
     ]));
@@ -70,9 +156,9 @@ function processPostRendering(data, node = document) {
  * @returns 
  */
 function runTasks(tasks) {
-  processPostRendering({ monitorIpv4, monitorIpv6, ipv4ScriptContent, ipv6ScriptContent });
+  processPostRendering({ monitorIpv4, monitorIpv6, ipv4ScriptContent, ipv6ScriptContent, history, currentPublicIpv4, currentPublicIpv6});
   return Promise.all(tasks).then(() => {
-    processPostRendering({ monitorIpv4, monitorIpv6, ipv4ScriptContent, ipv6ScriptContent });
+    processPostRendering({ monitorIpv4, monitorIpv6, ipv4ScriptContent, ipv6ScriptContent, history, currentPublicIpv4, currentPublicIpv6});
   });
 }
 
@@ -96,6 +182,21 @@ const save_ipv6_script_content = rpc.declare({
 	object: 'luci.public_ip_monitor',
 	method: 'save_ipv6_script_content',
   params: ["content"]
+});
+
+const get_history = rpc.declare({
+	object: 'luci.public_ip_monitor',
+	method: 'get_history'
+});
+
+const get_current_public_ipv4 = rpc.declare({
+	object: 'luci.public_ip_monitor',
+	method: 'get_current_public_ipv4'
+});
+
+const get_current_public_ipv6 = rpc.declare({
+	object: 'luci.public_ip_monitor',
+	method: 'get_current_public_ipv6'
 });
 
 return view.extend({
@@ -137,7 +238,10 @@ return view.extend({
   load: function() {
 		return Promise.all([
       load_ipv4_script_content(),
-      load_ipv6_script_content()
+      load_ipv6_script_content(),
+      get_history(),
+      get_current_public_ipv4(),
+      get_current_public_ipv6()
     ]);
 	},
   render: function (data) {
@@ -145,6 +249,9 @@ return view.extend({
 
     ipv4ScriptContent = (data[0] || {'content' : ''}).content;
     ipv6ScriptContent = (data[1] || {'content' : ''}).content;
+    history = data[2] || {};
+    currentPublicIpv4 = (data[3] || {'content' : ''}).content;
+    currentPublicIpv6 = (data[4] || {'content' : ''}).content;
 
     m = new form.Map(
       'public_ip_monitor',
@@ -152,6 +259,9 @@ return view.extend({
 			_('This monitors changes on the internet-facing public IP and trigger tasks when a change occurs.')
     );
     m.tabbed = true;
+
+    s = m.section(form.TypedSection, 'info', _('Overview'));
+    s.anonymous = true;
 
     // Setting up General Tab
 		s = m.section(form.TypedSection, 'general', _('General Settings'));
@@ -213,14 +323,6 @@ return view.extend({
     o.depends('monitor_ipv4', '1');
     o.depends('monitor_ipv6', '1');
 
-    o = s.option(form.Value, 'log_location', _('Log Location'), _('The location the log is stored.'));
-    o.optional = false;
-    o.datatype = 'string';
-    o.retain = true;
-    // make history depend on either ipv4 and ipv6 monitor on
-    o.depends('monitor_ipv4', '1');
-    o.depends('monitor_ipv6', '1');
-
     // Setting up IPv4 Tab
     s = m.section(form.TypedSection, 'ipv4', _('IPv4'));
     s.anonymous = true;
@@ -233,10 +335,6 @@ return view.extend({
     s = m.section(form.TypedSection, 'history', _('History'));
     s.anonymous = true;
 
-    // Setting up Logs Tab
-    s = m.section(form.TypedSection, 'logs', _('Logs'));
-    s.anonymous = true;
-
     return m.render().then((map) => {
       m
         .data
@@ -247,7 +345,7 @@ return view.extend({
           monitorIpv4 = data.general.monitor_ipv4
           monitorIpv6 = data.general.monitor_ipv6
 
-          setTimeout(() => processPostRendering({monitorIpv4, monitorIpv6, ipv4ScriptContent, ipv6ScriptContent}), 0);
+          setTimeout(() => processPostRendering({monitorIpv4, monitorIpv6, ipv4ScriptContent, ipv6ScriptContent, history, currentPublicIpv4, currentPublicIpv6}), 0);
         });
 
       return map;
